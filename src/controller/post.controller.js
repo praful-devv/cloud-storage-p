85301c658b8d9e1c670b22fc8619b59f8c2c2ac2
praful-devv <prafulanilwankhade3010@gmail.com>
2026-09-20 16:27:41 +0530

    imageKit connected and file uploded successfully

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,8 +1,20 @@
 const postModel = require("../models/post.model")
-
+const imagekit = require("@imagekit/nodejs")
+const {toFile} = require("@imagekit/nodejs")
+const IMAGEKIT = new imagekit({
+  private: process.env.IMAGEKIT_PRIVATE_KEY
+})
 const createpostController = async(req,res)=>{
 
-    console.log(req.file)
+    const file = await IMAGEKIT.files.upload({
+        file:await toFile(Buffer.from(req.file.buffer),'file'),
+        fileName:"xyz"
+    })
+
+    res.status(201).json({
+        message:"post upload",
+    })
+
 }
 
 module.exports = { createpostController };
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const postController = require("../controller/post.controller")
 const postRouter = express.Router()
 const multer = require("multer");
-const upload = new multer({ storage: multer.memoryStorage() });
+const upload =  multer({ storage: multer.memoryStorage() });
 
 postRouter.post("/",upload.single("post_image"),postController.createpostController)
 
